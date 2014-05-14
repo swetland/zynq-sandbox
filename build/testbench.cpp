0fea42525cdf15856f04e69a45b3af8b712aa741
Brian Swetland <swetland@frotz.net>
2014-05-14 14:30:57 -0700

    build: provide 4096x32 memory via DPI and a way to dump that memory

--- build/testbench.cpp
+++ build/testbench.cpp
@@ -21,10 +21,22 @@
 */
 
 #include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "Vtestbench.h"
 #include "verilated.h"
 #include <verilated_vcd_c.h>
+
+static unsigned memory[4096];
+
+void dpi_mem_write(int addr, int data) {
+	memory[addr & 0xFFF] = data;
+}
+
+void dpi_mem_read(int addr, int *data) {
+	*data = (int) memory[addr & 0xFFF];
+}
 
 #ifdef TRACE
 static vluint64_t now = 0;
@@ -36,6 +48,8 @@ double sc_time_stamp() {
 
 int main(int argc, char **argv) {
 	const char *vcdname = "trace.vcd";
+	const char *memname = NULL;
+	int fd;
 
 	while (argc > 1) {
 		if (!strcmp(argv[1], "-o")) {
@@ -52,6 +66,14 @@ int main(int argc, char **argv) {
 			fprintf(stderr,"error: no trace support\n");
 			return -1;
 #endif
+		} else if (!strcmp(argv[1], "-om")) {
+			if (argc < 3) {
+				fprintf(stderr, "error: -om requires argument\n");
+				return -1;
+			}
+			memname = argv[2];
+			argv += 2;
+			argc -= 3;
 		} else {
 			break;
 		}
@@ -84,5 +106,16 @@ int main(int argc, char **argv) {
 #endif
 	testbench->final();
 	delete testbench;
+
+	if (memname != NULL) {
+		fd = open(memname, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+		if (fd < 0) {
+			fprintf(stderr, "cannot open '%s' for writing\n", memname);
+			return -1;
+		}
+		write(fd, memory, sizeof(memory));
+		close(fd);
+	}
 	return 0;
 }
+
